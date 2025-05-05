2388108b5692b75afd9ee37d8f84a7da73d87e45
Kurosue <adityarahmadeni30@gmail.com>
2025-05-05 14:14:30 +0700

    refactor : add default contructor for use effect

--- include/effects/useEffects/Shadowstep.hpp
+++ include/effects/useEffects/Shadowstep.hpp
@@ -4,6 +4,14 @@
 class Shadowstep : public Effect
 {
 public:
+    Shadowstep()
+        : Effect("Shadowstep", "Dodge+15%, Haste×1.2, Acc×0.9", 2)
+    {
+        m_modifiers.dodgeChance = 0.15f;
+        m_modifiers.hasteMultiplier = 1.2f;
+        m_modifiers.accuracy = 0.9f;
+    }
+
     Shadowstep(float dodge, float hasteMul, float accMul, int dur)
         : Effect("Shadowstep",
                  "Dodge+" + std::to_string(int(dodge * 100)) + "%" + ", Haste×" +
